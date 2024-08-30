e82773aeb76c43032a51ae34bc6be61d2920b11f
roobeen-b <rubinbaidhya@gmail.com>
2024-08-30 13:22:25 +0545

    Initial configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)",
+        "very-dark-blue-dm": "hsl(207, 26%, 17%)",
+        "very-dark-blue-lm": "hsl(200, 15%, 8%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "very-dark-gray": "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        nunito: ["nunito", "sans-serif"],
       },
     },
   },
